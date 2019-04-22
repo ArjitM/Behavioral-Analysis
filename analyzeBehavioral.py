f6116af2d129b24762ed613ceb4c911924384f5d
Arjit Misra <arjit.misra@gmail.com>
2019-04-22 01:37:17 -0700

    histograms

--- analyzeBehavioral.py
+++ analyzeBehavioral.py
@@ -8,7 +8,7 @@ from itertools import groupby
 import seaborn
 import math
 
-filename = 'Results - 2019-04-18 9c907df8daad9f581330d99873708e27.txt'
+filenames = ['Results - 2019-04-18 9c907df8daad9f581330d99873708e27.txt']
 
 class ImageTypes(Enum):
 	Reward = "Reward"
@@ -23,6 +23,22 @@ class PumpStates(Enum):
 class Activity(Enum):
 	Running, Poking = auto(), auto()
 
+class Mouse:
+
+	def __init__(self, cageNum, rotation_intervals, poke_events):
+		self._cageNum = cageNum
+		self._rotation_intervals = rotation_intervals
+		self._poke_events = poke_events
+
+	@property
+	def rotation_intervals(self):
+		return self._rotation_intervals
+	
+	@property
+	def poke_events(self):
+		return self._poke_events
+	
+
 class RotationInterval:
 	#continguous series of wheel spins
 
@@ -31,7 +47,7 @@ class RotationInterval:
 		self._image = image
 		self.viable = True
 		self._rpms = [] #instantaneous speeds in rotations per minute
-		raw_rpms = [] #prone to error
+		raw_rpms = [] #prone to error, will be refined
 		for i in range(1, len(halfTimes) -1):
 			raw_rpms.append(60 /  (halfTimes[i+1] - halfTimes[i-1])) 
 		#instantaneous speeds at beginning and end of rotation event
@@ -75,7 +91,6 @@ class RotationInterval:
 	@property
 	def image(self):
 		return self._image
-	
 
 class PokeEvent:
 	#series of repeated pokes
@@ -93,6 +108,26 @@ class PokeEvent:
 			if p is PumpStates.On:
 				successful = True
 		return successful
+
+	def successfulPokes(self):
+		num = 0
+		for p in self._pumpStates:
+			if p is PumpStates.On:
+				num += 1
+		return num
+
+	def totalPokes(self):
+		return len(self._doorStates) // 2
+
+	def drinkTimes(self):
+		drinkStart = 0
+		drinkTimes = []
+		for p, t in zip(self._pumpStates, self._pumpTimes):
+			if p is PumpStates.On:
+				drinkStart = t
+			else:
+				drinkTimes.append(t - drinkStart)
+		return drinkTimes
 
 	@property
 	def startTime(self):
@@ -116,7 +151,7 @@ class Image:
 
 def cumulativeSuccess(poke_events):
 	outcomes = [int(pe.isSuccess()) for pe in poke_events]
-	times = [pe.startTime for pe in poke_events]
+	#times = [pe.startTime for pe in poke_events]
 	cumulative_success = 0
 	total = 0
 	cumulative_probabilities = []
@@ -124,9 +159,9 @@ def cumulativeSuccess(poke_events):
 		cumulative_success += outcome
 		total += 1
 		cumulative_probabilities.append(cumulative_success / total)
-	plt.plot(times, cumulative_probabilities)
+	plt.plot(cumulative_probabilities, marker='.')
 	plt.ylabel('Cumulative Probability')
-	plt.xlabel('Time')
+	plt.xlabel('Poke Events')
 	plt.title('Poke Success Rate')
 	plt.show()
 
@@ -151,13 +186,27 @@ def rpmTimeLapse(rotation_intervals, hour=None):
 		img = rot_ints_eachImage[0].image
 		datum, = plt.plot(allTimes, allSpeeds, marker='.', linestyle='None', label='{0} ({1})'.format(img.name, img.imageType.value))
 		data.append(datum)
-		# print('***', rot_int.halfTimes)
 	plt.xlabel('Time')
 	plt.ylabel('Speed in RPM')
 	# plt.xlim(0, 500)
 	plt.legend(handles=data)
 	plt.show()
 
+def numPokes(poke_events):
+	totalPokes = [pe.totalPokes() for pe in poke_events]
+	plt.hist(totalPokes)
+	plt.xlabel("Number of pokes per poke event")
+	plt.ylabel("Frequency")
+	plt.show()
+
+def drinkLengths(poke_events):
+	lengths = []
+	for pe in poke_events:
+		lengths.extend(pe.drinkTimes())
+	plt.hist(lengths)
+	plt.xlabel("Time (sec) drinking sugar water")
+	plt.ylabel("Frequency")
+	plt.show()
 
 def endRun(wheelHalfTimes, image, rotation_intervals):
 	if len(wheelHalfTimes) < 3:
@@ -175,71 +224,75 @@ def pruneRotationIntervals(rotation_intervals):
 	for ri in erratic:
 		rotation_intervals.remove(ri)
 	
-with open(filename, 'r') as resultFile:
-	allInput = resultFile.readlines()
-	currentImg = None
-	images = []
-	currentState = None
-	findFloat = re.compile("[+-]?([0-9]*[.])?[0-9]+") #regex to search for a number
-	wheelHalfTimes = []
-	doorStates = []
-	doorTimes = []
-	pumpStates = []
-	pumpTimes = []
-	poke_events = []
-	rotation_intervals = []
-	skipLine = False
-	for line in allInput:
-		if 'starting' in line:
-			continue
-		if 'Control image set:' in line:
-			for img in line[line.find('[')+1:line.rfind(']')].split(','):
-				images.append(Image(img.strip(), ImageTypes.Control))
-		elif 'Reward image set:' in line:
-			for img in line[line.find('[')+1:line.rfind(']')].split(','):
-				images.append(Image(img.strip(), ImageTypes.Reward))
-		elif 'Image' in line and 'Name:' in line:
-			curImgName = line[line.find('Name:') + 5: line.find(',')].strip()
-			currentImg = next((img for img in images if img.name == curImgName), None)
-			assert currentImg is not None, 'Unrecognized image: {0}'.format(curImgName)
-		elif 'Wheel' in line:
-			if skipLine:
-				#print('Skipping', line)
-				skipLine = False
-				continue
-			if currentState is Activity.Poking:
-				endPoke(doorStates, doorTimes, pumpTimes, pumpStates, currentImg, poke_events)
-				doorStates, doorTimes, pumpTimes, pumpStates = [], [], [], []
-			currentState = Activity.Running
-			if 'State:' in line:
-				wheelHalfTimes.append(float(findFloat.search(line).group(0))) #appends times
-			if 'revolution' in line:
-				#need to skip next data point because wheel state does not actually change
-				skipLine = True
-				continue #do NOT reset skipLine boolean
-		elif 'Pump' in line:
-			if currentState is Activity.Running:
-				endRun(wheelHalfTimes, currentImg, rotation_intervals)
-				wheelHalfTimes = []
-				currentState = Activity.Poking
-			pump_state = PumpStates.On if re.search("State: (.*), Time", line).group(1) == 'On' else PumpStates.Off
-			pumpStates.append(pump_state) 
-			pumpTimes.append(float(findFloat.search(line).group(0)))
-		elif 'Door' in line:
-			if currentState is Activity.Running:
-				endRun(wheelHalfTimes, currentImg, rotation_intervals)
-				wheelHalfTimes = []
-			currentState = Activity.Poking
-			door_state = DoorStates.High if re.search("State: (.*), Time", line).group(1) == 'High' else DoorStates.Low
-			doorStates.append(door_state)
-			doorTimes.append(float(findFloat.search(line).group(0)))
+
+
+for filename in filenames:
+	with open(filename, 'r') as resultFile:
+		allInput = resultFile.readlines()
+		currentImg = None
+		images = []
+		currentState = None
+		findFloat = re.compile("[+-]?([0-9]*[.])?[0-9]+") #regex to search for a number (float)
+		wheelHalfTimes = []
+		doorStates = []
+		doorTimes = []
+		pumpStates = []
+		pumpTimes = []
+		poke_events = []
+		rotation_intervals = []
 		skipLine = False
-	pruneRotationIntervals(rotation_intervals)
-	cumulativeSuccess(poke_events)
-	rpmTimeLapse(rotation_intervals)
+		for line in allInput:
+			if 'starting' in line:
+				continue
+			if 'Control image set:' in line:
+				for img in line[line.find('[')+1:line.rfind(']')].split(','):
+					images.append(Image(img.strip(), ImageTypes.Control))
+			elif 'Reward image set:' in line:
+				for img in line[line.find('[')+1:line.rfind(']')].split(','):
+					images.append(Image(img.strip(), ImageTypes.Reward))
+			elif 'Image' in line and 'Name:' in line:
+				curImgName = line[line.find('Name:') + 5: line.find(',')].strip()
+				currentImg = next((img for img in images if img.name == curImgName), None)
+				assert currentImg is not None, 'Unrecognized image: {0}'.format(curImgName)
+			elif 'Wheel' in line:
+				if skipLine:
+					skipLine = False
+					continue
+				if currentState is Activity.Poking:
+					endPoke(doorStates, doorTimes, pumpTimes, pumpStates, currentImg, poke_events)
+					doorStates, doorTimes, pumpTimes, pumpStates = [], [], [], []
+				currentState = Activity.Running
+				if 'State:' in line:
+					wheelHalfTimes.append(float(findFloat.search(line).group(0))) #appends times
+				if 'revolution' in line:
+					#need to skip next data point because wheel state does not actually change; it appears to be a bug
+					skipLine = True
+					continue #do NOT reset skipLine boolean
+			elif 'Pump' in line:
+				if currentState is Activity.Running:
+					endRun(wheelHalfTimes, currentImg, rotation_intervals)
+					wheelHalfTimes = []
+					currentState = Activity.Poking
+				pump_state = PumpStates.On if re.search("State: (.*), Time", line).group(1) == 'On' else PumpStates.Off
+				pumpStates.append(pump_state) 
+				pumpTimes.append(float(findFloat.search(line).group(0)))
+			elif 'Door' in line:
+				if currentState is Activity.Running:
+					endRun(wheelHalfTimes, currentImg, rotation_intervals)
+					wheelHalfTimes = []
+				currentState = Activity.Poking
+				door_state = DoorStates.High if re.search("State: (.*), Time", line).group(1) == 'High' else DoorStates.Low
+				doorStates.append(door_state)
+				doorTimes.append(float(findFloat.search(line).group(0)))
+			skipLine = False
+		pruneRotationIntervals(rotation_intervals)
+		cumulativeSuccess(poke_events)
+		rpmTimeLapse(rotation_intervals)
+		numPokes(poke_events)
+		drinkLengths(poke_events)
 
 
-			
+				
 
 
 
